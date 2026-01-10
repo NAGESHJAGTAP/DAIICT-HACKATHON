5b57013bce15734cdf0fa66191d26c5ad719c31f
Nageshjagtap <nageshjagtap063@gmail.com>
2026-01-10 08:16:59 +0530

    poll Inngest run status and fetch AI resume analysis output

--- ai-career-coach/app/api/ai-resume-agent/route.ts
+++ ai-career-coach/app/api/ai-resume-agent/route.ts
@@ -82,3 +82,13 @@ if (!output) {
 
 
 }
+
+   async function getRuns(runId: string) {
+    const result = await axios.get(`${process.env.INNGEST_SERVER_HOST}/v1/events/${runId}/runs`,{
+            headers:{
+                    Authorization: `Bearer ${process.env.INNGEST_SIGNING_KEY}`
+            }
+    }
+    )
+    return result.data;
+}
